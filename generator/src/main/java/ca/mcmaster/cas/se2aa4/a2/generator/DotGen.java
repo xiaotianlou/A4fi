0a37070873db4b0232b83aeee84290183c626fb5
xiaotian Lou <loux8@mcmaster.ca>
2023-02-11 11:58:11 -0500

    step1 segmetns list create

--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DotGen.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DotGen.java
@@ -1,10 +1,11 @@
 package ca.mcmaster.cas.se2aa4.a2.generator;
 
-import java.io.IOException;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.Random;
 
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Segment;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Vertex;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Property;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
@@ -26,6 +27,7 @@ public class DotGen {
                 vertices.add(Vertex.newBuilder().setX((double) x+square_size).setY((double) y+square_size).build());
             }
         }
+
         // Distribute colors randomly. Vertices are immutable, need to enrich them
         Set<Vertex> verticesWithColors = new HashSet<>();
         Random bag = new Random();
@@ -39,7 +41,24 @@ public class DotGen {
             verticesWithColors.add(colored);
         }
 
-        return Mesh.newBuilder().addAllVertices(verticesWithColors).build();
+        HashSet<Segment> segments = new HashSet<>();
+
+
+
+        return Mesh.newBuilder().addAllVertices(verticesWithColors).addAllSegments(segments).build();
     }
+    private int findVertex(List<Vertex> vertexList,double x,double y){
+        int i=0;
+        for (Vertex v:vertexList){
+            if(v.getX()==x&&v.getY()==y){
+                return i;
+            }
+            i++;
+        }
+        return -1;
+
+    }
+
+
 
 }
