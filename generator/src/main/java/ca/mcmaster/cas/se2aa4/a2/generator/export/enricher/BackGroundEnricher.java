16105f7ac691d3cace757b2879aaf3fb00e3f031
xiaotian Lou <loux8@mcmaster.ca>
2023-03-23 11:53:59 -0400

    background of island

--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/export/enricher/BackGroundEnricher.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/export/enricher/BackGroundEnricher.java
@@ -1,39 +1,38 @@
 package ca.mcmaster.cas.se2aa4.a2.generator.export.enricher;
 
 import ca.mcmaster.cas.se2aa4.a2.io.Structs;
-import java.util.Random;
 
-public class RandomEnricher implements Enricher {
+import java.awt.*;
 
-    private final Random bag = new Random();
-    private final float threshold;
+/**
+ * @author loux8@mcmaster.ca
+ * @date 2023/3/23 11:30
+ */
+public class BackGroundEnricher implements Enricher{
 
-    public RandomEnricher(float threshold) {
-        this.threshold = threshold;
+    Color backgound = new Color(0,0,255);
+
+    public void changeBackGound(Color newBackGround){
+        this.backgound=newBackGround;
     }
+
 
     @Override
     public Structs.Mesh process(Structs.Mesh aMesh) {
 
         Structs.Mesh.Builder clone = Structs.Mesh.newBuilder();
-
         clone.addAllVertices(aMesh.getVerticesList());
         clone.addAllSegments(aMesh.getSegmentsList());
 
         for(Structs.Polygon poly: aMesh.getPolygonsList()) {
-
-            float level = bag.nextFloat();
-
             Structs.Polygon.Builder pc = Structs.Polygon.newBuilder(poly);
 
-            if (level < threshold){
-                String color = bag.nextInt(255)+","+bag.nextInt(255)+","+bag.nextInt(255);
+                String color = backgound.getRed()+","+backgound.getGreen()+","+backgound.getBlue();
                 Structs.Property p = Structs.Property.newBuilder()
                         .setKey("rgb_color")
                         .setValue(color)
                         .build();
                 pc.addProperties(p);
-            }
             clone.addPolygons(pc);
         }
         return clone.build();
--- island/src/main/java/Main.java
+++ island/src/main/java/Main.java
@@ -2,22 +2,27 @@
 
 import ca.mcmaster.cas.se2aa4.a2.generator.adt.Mesh;
 import ca.mcmaster.cas.se2aa4.a2.generator.configuration.Configuration;
+import ca.mcmaster.cas.se2aa4.a2.generator.export.Exporter;
+import ca.mcmaster.cas.se2aa4.a2.generator.export.enricher.BackGroundEnricher;
+import ca.mcmaster.cas.se2aa4.a2.generator.export.enricher.RandomEnricher;
 import ca.mcmaster.cas.se2aa4.a2.generator.specification.Buildable;
 import ca.mcmaster.cas.se2aa4.a2.generator.specification.SpecificationFactory;
+import ca.mcmaster.cas.se2aa4.a2.io.MeshFactory;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * @author loux8@mcmaster.ca
  * @date ${DATE} ${TIME}
  */
 public class Main {
-    public static void main(String[] args) {
-        Configuration config = new Configuration(args);
-        Buildable specification = SpecificationFactory.create(config);
-        Mesh theMesh = specification.build();
+    public static void main(String[] args) throws IOException {
 
+        Structs.Mesh exported = new MeshFactory().read("img/input.mesh");
+            exported = new BackGroundEnricher().process(exported);
 
+        new MeshFactory().write(exported, "outputwithbackground.mesh");
     }
 }
