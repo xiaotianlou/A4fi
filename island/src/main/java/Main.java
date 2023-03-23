dff31c7e99def945a6f59b4e78f7f1f405961402
xiaotian Lou <loux8@mcmaster.ca>
2023-03-23 14:56:49 -0400

    MVP with sand edge

--- island/src/main/java/Main.java
+++ island/src/main/java/Main.java
@@ -1,5 +1,3 @@
-
-
 import ca.mcmaster.cas.se2aa4.a2.generator.adt.Mesh;
 import ca.mcmaster.cas.se2aa4.a2.generator.adt.Polygon;
 import ca.mcmaster.cas.se2aa4.a2.generator.adt.Vertex;
@@ -7,14 +5,12 @@ import ca.mcmaster.cas.se2aa4.a2.generator.configuration.Configuration;
 import ca.mcmaster.cas.se2aa4.a2.generator.export.Exporter;
 import ca.mcmaster.cas.se2aa4.a2.generator.export.enricher.BackGroundEnricher;
 import ca.mcmaster.cas.se2aa4.a2.generator.export.enricher.CircleIslandEnricher;
-import ca.mcmaster.cas.se2aa4.a2.generator.export.enricher.RandomEnricher;
 import ca.mcmaster.cas.se2aa4.a2.generator.specification.Buildable;
 import ca.mcmaster.cas.se2aa4.a2.generator.specification.SpecificationFactory;
 import ca.mcmaster.cas.se2aa4.a2.io.MeshFactory;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs;
 
 import java.awt.*;
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -33,30 +29,41 @@ public class Main {
         Mesh theMesh = specification.build();
 
         //get centreid of centre poly relax
-        int cen_x=1920/2;
-        int cen_y=1920/2;
-        int circle_size =800;
-        List<Vertex> centre_bag=new ArrayList<Vertex>();
-        List<Vertex> centre_bag2=new ArrayList<Vertex>();
+        int cen_x = 1920 / 2;
+        int cen_y = 1920 / 2;
+        int circle_size = 800;
+        List<Vertex> centre_bag = new ArrayList<Vertex>();
+        List<Vertex> centre_bag2 = new ArrayList<Vertex>();
+        List<Vertex> centre_bag3 = new ArrayList<Vertex>();
+        List<Vertex> centre_bag4 = new ArrayList<Vertex>();
+        double space = Math.sqrt(1920 * 1920 / 1000);
 
-        for (Polygon p :theMesh.getPolygons()){
-            float x =p.centroid().x();
-            float y =p.centroid().y();
+
+        for (Polygon p : theMesh.getPolygons()) {
+            float x = p.centroid().x();
+            float y = p.centroid().y();
             //先找
-            double distance = Math.sqrt(Math.pow(x-cen_x,2)+Math.pow(y-cen_y,2));
-            if(distance<circle_size){
+            double distance = Math.sqrt(Math.pow(x - cen_x, 2) + Math.pow(y - cen_y, 2));
+            if (distance < circle_size) {
                 centre_bag.add(p.centroid());
-                if(distance<circle_size/2){
+                if (distance < circle_size - space) {
                     centre_bag2.add(p.centroid());
+                    if (distance < circle_size / 2) {
+                        centre_bag3.add(p.centroid());
+                        if (distance < circle_size / 2 - space) {
+                            centre_bag4.add(p.centroid());
+                        }
+                    }
                 }
             }
         }
 
-        Structs.Mesh exported =new Exporter().run(theMesh);
+        Structs.Mesh exported = new Exporter().run(theMesh);
         exported = new BackGroundEnricher(new Color(13, 108, 185)).process(exported);
-        exported = new CircleIslandEnricher(new Color(153, 220, 211),centre_bag).process(exported);
-        exported = new CircleIslandEnricher(new Color(146, 198, 250),centre_bag2).process(exported);
-
+        exported = new CircleIslandEnricher(new Color(153, 220, 211), centre_bag).process(exported);
+        exported = new CircleIslandEnricher(new Color(255, 255, 255), centre_bag2).process(exported);
+        exported = new CircleIslandEnricher(new Color(153, 220, 211), centre_bag3).process(exported);
+        exported = new CircleIslandEnricher(new Color(146, 198, 250), centre_bag4).process(exported);
 
         new MeshFactory().write(exported, "og.mesh");
     }
