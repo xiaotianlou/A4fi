3f20b82625096ccd572d323f1baff416da94b095
xiaotian Lou <loux8@mcmaster.ca>
2023-03-24 12:06:48 -0400

    name error fix

--- island/src/main/java/Main.java
+++ island/src/main/java/Main.java
@@ -16,9 +16,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
         //MVP
         Configuration config = new Configuration("generator/generator.jar -k irregular -h 1920 -w 1920 -p 1000 -r 5 -o ireg.mesh".split(" "));
+
+
         Buildable specification = SpecificationFactory.create(config);
         Mesh theMesh = specification.build();
         LagoonRenderer mvp = new LagoonRenderer(theMesh);
         new MeshFactory().write(mvp.Rendering(), "og.mesh");//
+
+
     }
 }
