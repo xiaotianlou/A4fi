a4ce8062499e0ac3ee94776c0d0658fb6877d70e
Changhao Wu <wu399@mcmaster.ca>
2023-03-25 13:05:55 -0400

    reading mesh from io

--- island/src/main/java/Main.java
+++ island/src/main/java/Main.java
@@ -3,8 +3,14 @@ import ca.mcmaster.cas.se2aa4.a2.generator.configuration.Configuration;
 import ca.mcmaster.cas.se2aa4.a2.generator.specification.Buildable;
 import ca.mcmaster.cas.se2aa4.a2.generator.specification.SpecificationFactory;
 import ca.mcmaster.cas.se2aa4.a2.io.MeshFactory;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs;
 import featureRenderer.LagoonRenderer;
 import featureRenderer.Renderer;
+import transformation.builtinADT.MeshADT;
+import transformation.importation.Importer;
+import transformation.importation.polygonImporter;
+import transformation.importation.segmentImporter;
+import transformation.importation.vertexImporter;
 
 import java.io.IOException;
 
@@ -14,12 +20,26 @@ import java.io.IOException;
  */
 public class Main {
     public static void main(String[] args) throws IOException {
+
+        MeshADT meshADT = new MeshADT();
+        String input = args[0];
+        Structs.Mesh aMesh = new MeshFactory().read(input);
+
+        Importer polygonImporter = new polygonImporter();
+        Importer segmentImporter = new segmentImporter();
+        Importer vertexImporter = new vertexImporter();
+        vertexImporter.read(aMesh,meshADT);
+        segmentImporter.read(aMesh,meshADT);
+        polygonImporter.read(aMesh,meshADT);
+
+
+
+
         //MVP
-        Configuration config = new Configuration("generator/generator.jar -k irregular -h 1920 -w 1920 -p 1000 -r 5 -o ireg.mesh".split(" "));
-        Buildable specification = SpecificationFactory.create(config);
-        Mesh theMesh = specification.build();
-        LagoonRenderer mvp = new LagoonRenderer(theMesh);
-        new MeshFactory().write(mvp.Rendering(), "og.mesh");//
+//        Configuration config = new Configuration("generator/generator.jar -k irregular -h 1920 -w 1920 -p 1000 -r 5 -o ireg.mesh".split(" "));
+//        Mesh theMesh = specification.build();
+//        LagoonRenderer mvp = new LagoonRenderer(theMesh);
+//        new MeshFactory().write(mvp.Rendering(), "og.mesh");//
 
 
     }
