cbea3c0a615d5967e7b002e953b330631649a52c
xiaotian Lou <loux8@mcmaster.ca>
2023-03-26 17:42:37 -0400

    finished and tested ElevationRenderer

---
+++ island/src/main/java/featureRenderer/Elevation/BasicElevationGenerator.java
@@ -0,0 +1,34 @@
+package featureRenderer.Elevation;
+
+import Reproducibility.Seed;
+import featureRenderer.Generable;
+import transformation.builtinADT.InfoSet;
+import transformation.builtinADT.MeshADT;
+import transformation.builtinADT.PolygonADT;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BasicElevationGenerator implements Generable {
+    @Override
+    public MeshADT Genering(MeshADT m, Seed seed) {
+
+        List<PolygonADT> land = new ArrayList();
+
+
+        for (PolygonADT p : m.getPolygons()) {
+            if (p.getInfoSet().isIsland()) {
+                land.add(p);
+            }
+        }
+        for (PolygonADT p : land) {
+            InfoSet ti = p.getInfoSet();
+
+            if (ti.getElevation() == -1) {
+                ti.setElevation(5+seed.getSeedArray().get(p.getId() % seed.getSeedArray().size()) * 40 / 10);
+            }
+        }
+        return m;
+
+    }
+}
