24bc781e7e635418262a1ee2929675a42ebfd65a
xiaotian Lou <loux8@mcmaster.ca>
2023-03-26 16:00:57 -0400

    get island

--- island/src/main/java/featureRenderer/ElevationRenderer.java
+++ island/src/main/java/featureRenderer/ElevationRenderer.java
@@ -4,6 +4,10 @@ import Reproducibility.Seed;
 import ca.mcmaster.cas.se2aa4.a2.generator.adt.Mesh;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs;
 import transformation.builtinADT.MeshADT;
+import transformation.builtinADT.PolygonADT;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author loux8@mcmaster.ca
@@ -14,9 +18,21 @@ public class ElevationRenderer implements Renderable{
 
     @Override
     public MeshADT Rendering(MeshADT m, Seed seed) {
+        List island = new ArrayList();
+
+
+        for (PolygonADT p:m.getPolygons()){
+            if(p.isIsland()){
+                island.add(p);
+            }
+        }
+
+
+
+
+
+
+
         return null;
-
-
-
     }
 }
