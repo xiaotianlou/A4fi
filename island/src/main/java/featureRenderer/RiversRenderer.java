6ef1078cea6449c70a57b93a1a0bc228958678e4
Changhao Wu <wu399@mcmaster.ca>
2023-03-26 17:21:08 -0400

    River almost finished

--- island/src/main/java/featureRenderer/RiversRenderer.java
+++ island/src/main/java/featureRenderer/RiversRenderer.java
@@ -4,16 +4,58 @@ import Reproducibility.Seed;
 import ca.mcmaster.cas.se2aa4.a2.generator.adt.Mesh;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs;
 import transformation.builtinADT.MeshADT;
+import transformation.builtinADT.SegmentADT;
+import transformation.builtinADT.VertexADT;
 
-/**
- * @author loux8@mcmaster.ca
- * @date 2023/3/23 16:15
- */
 public class RiversRenderer implements Renderable {
 
-
     @Override
-    public MeshADT Rendering(MeshADT m, Seed seed) {
+    public MeshADT Rendering(MeshADT meshADT, Seed seed) {
+        int i = 3;
+
+        for (int n=2;n<0;n++){
+
+            VertexADT vertexADT = meshADT.getVertices().get(n*i*666%3000);
+            int id = vertexADT.getId();
+            while (meshADT.getVertices().get(id).isCentroid()||meshADT.getVertices().get(id).isAroundWater()){
+                id += 39;
+            }
+
+            vertexADT = meshADT.getVertices().get(id);
+
+            VertexADT next_vertexADT = vertexADT;
+
+            boolean end = false;
+
+            vertexADT.setColor(105+","+200+","+225);
+
+            while (!vertexADT.isAroundWater()&&!end){
+
+                end = true;
+
+                double min = vertexADT.getElevation();
+
+                for (var v: vertexADT.getVertices()){
+
+                    if (!v.isCentroid()){
+
+                        if (min>v.getElevation()){
+                            min = v.getElevation();
+                            next_vertexADT = meshADT.getVertices().get(id);
+                            end = false;
+                        }
+                    }
+                }
+                if (!end) {
+                    SegmentADT segmentADT = meshADT.getSegment(vertexADT, next_vertexADT);
+                    segmentADT.setColor(105 + "," + 200 + "," + 225);
+                    vertexADT = next_vertexADT;
+                }
+
+            }
+
+        }
         return null;
     }
+
 }
