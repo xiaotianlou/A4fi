c54509189e32d5853edc2d1cefec550aaa57fd4f
ljm-jim <ljm-jim@hotmail.com>
2023-03-26 21:23:01 -0400

    Biome

--- island/src/main/java/transformation/builtinADT/Biome.java
+++ island/src/main/java/transformation/builtinADT/Biome.java
@@ -1,7 +1,7 @@
 package transformation.builtinADT;
 
 public enum Biome {
-    None,
+    Tropical_Rain_Forest, Tropical_Seasonal_Forest, Temperate_Rain_Forest, Temperate_Deciduous_Forest, None, Temperate_Grassland_and_Desert, Taiga, Tundra, Subtropical_Desert
 
 
 }
