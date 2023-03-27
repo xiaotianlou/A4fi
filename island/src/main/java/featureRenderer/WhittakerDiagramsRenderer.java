51b4038d2fc4e8d2a341d83587bb186a306c81ba
xiaotian Lou <loux8@mcmaster.ca>
2023-03-26 21:43:59 -0400

    random seed generator for help

--- island/src/main/java/featureRenderer/WhittakerDiagramsRenderer.java
+++ island/src/main/java/featureRenderer/WhittakerDiagramsRenderer.java
@@ -1,11 +1,14 @@
 package featureRenderer;
 
+import Reproducibility.Seed;
 import transformation.builtinADT.MeshADT;
 import transformation.builtinADT.PolygonADT;
 
 import java.util.Random;
 
 public class WhittakerDiagramsRenderer {
+
+
     public MeshADT WhittakerDiagramGenerator(MeshADT mesh, String DiagramType){
         for (PolygonADT p: mesh.getPolygons()){
             for(PolygonADT neighbour: p.getPolygons())
@@ -15,9 +18,13 @@ public class WhittakerDiagramsRenderer {
         }
         return mesh;
     }
-    public PolygonADT BoundryColor(PolygonADT p, String DiagramType){
+    public PolygonADT BoundryColor(PolygonADT p, String DiagramType,Seed seed){
         Random random = new Random();
-        int ran = random.nextInt(3);
+
+        int ran = seed.getSeedArray().get(p.getId()%seed.getSeedArray().size())%3;
+
+
+
         if(ran > 0.75 ){
             p.setColor("238,118,33");
         }else{
--- island/src/test/java/MainTest.java
+++ island/src/test/java/MainTest.java
@@ -28,7 +28,7 @@ class MainTest {
     @Test
     void Etolake() throws IOException {
         MeshADT meshADT = new MeshADT();
-        String input_c = "C:\\Users\\22091\\IdeaProjects\\a2---mesh-generator-team-28_new1\\IOArea\\inputoff.mesh";
+        String input_c = "C:\\Users\\22091\\IdeaProjects\\a2---mesh-generator-team-28_newnewnewn\\IOArea\\inputoff.mesh";
         Structs.Mesh aMesh = new MeshFactory().read(input_c);
 
         Importer polygonImporter = new polygonImporter();
@@ -47,7 +47,7 @@ class MainTest {
         meshADT = new ShapeRenderer().Rendering(meshADT,s);
         meshADT = new ElevationRenderer().Rendering(meshADT,s);
         Structs.Mesh output = meshADT.toMesh();
-        new MeshFactory().write(output, "C:\\Users\\22091\\IdeaProjects\\a2---mesh-generator-team-28_new1\\IOArea\\lagtest.mesh");//
+        new MeshFactory().write(output, "C:\\Users\\22091\\IdeaProjects\\a2---mesh-generator-team-28_newnewnewn\\IOArea\\lagtest.mesh");//
 
 //java -jar visualizer/visualizer.jar -i C:\Users\22091\IdeaProjects\a2---mesh-generator-team-28_new1\IOArea\lagtest.mesh -o C:\Users\22091\IdeaProjects\a2---mesh-generator-team-28_new1\IOArea\lagtest1.svg -x
 
@@ -107,7 +107,7 @@ class MainTest {
 //    s= new Seed(802517);
         meshADT = new ShapeRenderer().Rendering(meshADT,s);
         Structs.Mesh output = meshADT.toMesh();
-        new MeshFactory().write(output, "C:\\Users\\22091\\IdeaProjects\\a2---mesh-generator-team-28_new1\\IOArea\\lagtest.mesh");//
+        new MeshFactory().write(output, "C:\\Users\\22091\\IdeaProjects\\a2---mesh-generator-team-28_newnewnewn\\IOArea\\lagtest.mesh");//
 //        java -jar visualizer/visualizer.jar -i C:\Users\22091\IdeaProjects\a2---mesh-generator-team-28_new1\IOArea\lagtest.mesh -o C:\Users\22091\IdeaProjects\a2---mesh-generator-team-28_new1\IOArea\lagtest.svg -x
 
     }
@@ -131,7 +131,7 @@ class MainTest {
         meshADT = new ShapeRenderer().Rendering(meshADT,s);
         meshADT = new ElevationRenderer().Rendering(meshADT,s);
         Structs.Mesh output = meshADT.toMesh();
-        new MeshFactory().write(output, "C:\\Users\\22091\\IdeaProjects\\a2---mesh-generator-team-28_new1\\IOArea\\lagtest.mesh");//
+        new MeshFactory().write(output, "C:\\Users\\22091\\IdeaProjects\\a2---mesh-generator-team-28_newnewnewn\\IOArea\\lagtest.mesh");//
 //java -jar visualizer/visualizer.jar -i C:\Users\22091\IdeaProjects\a2---mesh-generator-team-28_new1\IOArea\lagtest.mesh -o C:\Users\22091\IdeaProjects\a2---mesh-generator-team-28_new1\IOArea\lagtest1.svg -x
 
         new Color(22, 241, 140);
@@ -181,7 +181,7 @@ class MainTest {
             }
         }
         Structs.Mesh output2 = meshADT.toMesh();
-        new MeshFactory().write(output2, "C:\\Users\\22091\\IdeaProjects\\a2---mesh-generator-team-28_new1\\IOArea\\lagtestH.mesh");//
+        new MeshFactory().write(output2, "C:\\Users\\22091\\IdeaProjects\\a2---mesh-generator-team-28_newnewnewn\\IOArea\\lagtestH.mesh");//
 //java -jar visualizer/visualizer.jar -i C:\Users\22091\IdeaProjects\a2---mesh-generator-team-28_new1\IOArea\lagtestH.mesh -o C:\Users\22091\IdeaProjects\a2---mesh-generator-team-28_new1\IOArea\lagtest1H.svg -x
 
 
