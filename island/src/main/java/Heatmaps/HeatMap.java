f5398a94469a6a3907a4069db863391a165bcb15
xiaotian Lou <loux8@mcmaster.ca>
2023-03-27 22:34:31 -0400

    eliminationOfWaters

--- island/src/main/java/Heatmaps/HeatMap.java
+++ island/src/main/java/Heatmaps/HeatMap.java
@@ -6,20 +6,19 @@ import transformation.builtinADT.InfoSet;
 import transformation.builtinADT.MeshADT;
 import transformation.builtinADT.PolygonADT;
 
+import java.awt.*;
+
 public abstract class HeatMap implements Buildable{
     MeshADT m;
     public void eliminationOfWaters(){
         InfoSet temp;
+        Color c = new Color(0,0,0);
         for (PolygonADT p : m.getPolygons()){
             temp= p.getInfoSet();
             if(!temp.isIsland()){
-
-
+            temp.setColor(c);
             }
-
         }
-
-
     }
 
 
