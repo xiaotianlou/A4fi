06c949aeff9e12caef481fa123febdf03a4753d4
xiaotian Lou <loux8@mcmaster.ca>
2023-03-27 22:32:43 -0400

    HeatMap and BiomeRenderer Normalization

--- island/src/main/java/Heatmaps/HeatMap.java
+++ island/src/main/java/Heatmaps/HeatMap.java
@@ -2,10 +2,26 @@ package Heatmaps;
 
 import Reproducibility.Seed;
 import featureRenderer.Renderable;
+import transformation.builtinADT.InfoSet;
 import transformation.builtinADT.MeshADT;
+import transformation.builtinADT.PolygonADT;
 
 public abstract class HeatMap implements Buildable{
     MeshADT m;
+    public void eliminationOfWaters(){
+        InfoSet temp;
+        for (PolygonADT p : m.getPolygons()){
+            temp= p.getInfoSet();
+            if(!temp.isIsland()){
+
+
+            }
+
+        }
+
+
+    }
+
 
     public HeatMap(MeshADT m) {
         this.m = m;
--- island/src/main/java/featureRenderer/BiomeRenderer.java
+++ island/src/main/java/featureRenderer/BiomeRenderer.java
@@ -2,6 +2,7 @@ package featureRenderer;
 
 import Reproducibility.Seed;
 import transformation.builtinADT.Biome;
+import transformation.builtinADT.InfoSet;
 import transformation.builtinADT.MeshADT;
 import transformation.builtinADT.PolygonADT;
 
@@ -16,9 +17,11 @@ public class BiomeRenderer implements Renderable {
 
     @Override
     public MeshADT Rendering(MeshADT m, Seed seed) {
-        for (PolygonADT p : m.getPolygons()) {
+        InfoSet p;
+        for (PolygonADT poly : m.getPolygons()) {
+            p=poly.getInfoSet();
             int temperature = p.getTemperature();
-            int humidity = p.getHumidity();
+            int humidity = poly.getHumidity();
             if (!p.isIsland()) {
                 continue;
             }
@@ -26,62 +29,62 @@ public class BiomeRenderer implements Renderable {
             if (20 <= temperature) {
                 if (humidity >= 250) {
                     p.setBiome(Biome.Tropical_Rain_Forest);
-                    setColor(p, new Color(0, 255, 0));
+                    p.setColor(new Color(0, 255, 0));
                 } else if (humidity >= 50) {
                     p.setBiome(Biome.Tropical_Seasonal_Forest);
-                    setColor(p, new Color(154, 205, 50));
+                    p.setColor( new Color(154, 205, 50));
                 }else {
                     p.setBiome(Biome.Subtropical_Desert);
-                    setColor(p, new Color(238, 118, 33));
+                    p.setColor( new Color(238, 118, 33));
                 }
             } else if (temperature >= 10) {
                 if (humidity >= 225) {
                     p.setBiome(Biome.Temperate_Rain_Forest);
-                    setColor(p, new Color(0, 250, 154));
+                    p.setColor( new Color(0, 250, 154));
 
                 } else if (humidity >= 125) {
                     p.setBiome(Biome.Temperate_Deciduous_Forest);
-                    setColor(p, new Color(46, 139, 87));
+                    p.setColor( new Color(46, 139, 87));
 
                 } else if (humidity >= 25) {
                     p.setBiome(Biome.Temperate_Grassland_and_Desert);
-                    setColor(p, new Color(255, 215, 0));
+                    p.setColor( new Color(255, 215, 0));
                 }else {
                     p.setBiome(Biome.Subtropical_Desert);
-                    setColor(p, new Color(238, 118, 33));
+                    p.setColor( new Color(238, 118, 33));
                 }
             } else if (temperature >= 3) {
                 if (humidity >= 200) {
                     p.setBiome(Biome.Temperate_Rain_Forest);
-                    setColor(p, new Color(0, 250, 154));
+                    p.setColor( new Color(0, 250, 154));
 
                 } else if (humidity >= 75) {
                     p.setBiome(Biome.Temperate_Deciduous_Forest);
-                    setColor(p, new Color(46, 139, 87));
+                    p.setColor( new Color(46, 139, 87));
 
                 } else if (humidity >= 15) {
                     p.setBiome(Biome.Temperate_Grassland_and_Desert);
-                    setColor(p, new Color(255, 215, 0));
+                    p.setColor( new Color(255, 215, 0));
                 }else {
                     p.setBiome(Biome.Subtropical_Desert);
-                    setColor(p, new Color(238, 118, 33));
+                    p.setColor( new Color(238, 118, 33));
                 }
             } else if (temperature >= -5) {
                 if (humidity >= 30) {
                     p.setBiome(Biome.Taiga);
-                    setColor(p, new Color(0, 100, 0));
+                    p.setColor( new Color(0, 100, 0));
 
                 } else if (humidity >= 10) {
                     p.setBiome(Biome.Temperate_Grassland_and_Desert);
-                    setColor(p, new Color(255, 215, 0));
+                    p.setColor( new Color(255, 215, 0));
                 }else {
                     p.setBiome(Biome.Subtropical_Desert);
-                    setColor(p, new Color(238, 118, 33));
+                    p.setColor(new Color(238, 118, 33));
 
                 }
             } else if (temperature < -5) {
                 p.setBiome(Biome.Tundra);
-                setColor(p, new Color(0, 206, 209));
+                p.setColor( new Color(0, 206, 209));
 
             }
         }
