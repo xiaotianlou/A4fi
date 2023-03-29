270ad07174c62438f841dec0f1f90ffcf4232357
Changhao Wu <wu399@mcmaster.ca>
2023-03-28 22:17:59 -0400

    Working on the ElevationHeatMap

--- island/src/main/java/Heatmaps/ElevationHeatMap.java
+++ island/src/main/java/Heatmaps/ElevationHeatMap.java
@@ -2,14 +2,33 @@ package Heatmaps;
 
 import transformation.builtinADT.MeshADT;
 
+import java.awt.*;
+
 public class ElevationHeatMap extends HeatMap{
-    public ElevationHeatMap(MeshADT m) {
-        super(m);
+//    public ElevationHeatMap(MeshADT m) {
+//        super(m);
+//    }
+
+    public ElevationHeatMap(MeshADT meshADT) {
+        super(meshADT);
+        heatMapInitialization();
+        build(this.meshADT);
     }
 
     @Override
     public void build(MeshADT meshADT) {
-
+        double max=0;
+        for (var p: meshADT.getPolygons()){
+            if (p.getElevation()>max){
+                max = p.getElevation();
+            }
+        }
+        for (var p: meshADT.getPolygons()){
+            double n = p.getElevation()/max;
+            Color color=new Color((int)(50*n), (int)(50*n), 255);
+            p.getInfoSet().setColor(color);
+        }
     }
+
 
 }
