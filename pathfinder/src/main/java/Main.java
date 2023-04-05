efb40a679cf508dfeb8ce50d461fd134de602bee
xiaotian Lou <loux8@mcmaster.ca>
2023-04-05 12:14:41 -0400

    create module

---
+++ pathfinder/src/main/java/Main.java
@@ -0,0 +1,11 @@
+
+
+/**
+ * @author loux8@mcmaster.ca
+ * @date ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ pathfinder/src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author loux8@mcmaster.ca
+ * @date 2023/4/5 12:03
+ */
+public class MainTest {
+    @Test
+    void main() {
+    }
+
+}
