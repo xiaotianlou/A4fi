52564dd5acc43e737284d49b02795800bb0b0c04
xiaotian Lou <loux8@mcmaster.ca>
2023-04-10 22:09:25 -0400

    beforeTest

--- pathfinder/src/test/java/MainTest.java
+++ pathfinder/src/test/java/MainTest.java
@@ -1,22 +1,34 @@
 import Graph.Nodes;
+import ca.mcmaster.cas.se2aa4.a2.io.MeshFactory;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 
 /**
  * @author loux8@mcmaster.ca
  * @date 2023/4/5 12:03
  */
 public class MainTest {
+
+    @BeforeEach
+    void initial() throws IOException {
+        MeshADT meshADT = new MeshADT();
+        String input_c = "..//IOArea\\inputoff.mesh";
+        Structs.Mesh aMesh = new MeshFactory().read(input_c);
+        meshADT.readInputMesh(aMesh);
+
+        int seedint = (int) (Math.random() * 1000000);
+        System.out.println("seed is:" + seedint);
+        this.s = new Seed(seedint);
+    }
     @Test
     void main() {
-        Nodes tail=new Nodes(1,1,1,null);
-        tail=null;
-        if (tail==null){
-            System.out.println("1");
-        }
-        else {
 
-            System.out.println("2");
-        }
+
+
+
     }
 
 }
