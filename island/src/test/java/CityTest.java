bd8cc62886a113e32ba9be56f65a6e83ed52b106
xiaotian Lou <loux8@mcmaster.ca>
2023-04-11 15:44:48 -0400

    step2 idea and test

--- island/src/test/java/CityTest.java
+++ island/src/test/java/CityTest.java
@@ -1,5 +1,7 @@
+import Reproducibility.Seed;
 import ca.mcmaster.cas.se2aa4.a2.io.MeshFactory;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs;
+import featureRenderer.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,15 +14,38 @@ public class CityTest {
     MeshADT meshADT1;
 
     @Test
-    public void city() {
+    public void cityIdea() {
 
         //        new Color(123, 183, 64);
-       SegmentADT s = meshADT1.getSegment(meshADT1.getVertex(500,500),meshADT1.getVertex(500,500));
-       s.setColor(new int[]{123, 183, 64});
-       s.setThickness(100);
+        SegmentADT s = meshADT1.getSegment(meshADT1.getVertex(500, 500), meshADT1.getVertex(500, 500));
+        s.setColor(new int[]{123, 183, 64});
+        s.setThickness(30);
+
+    }
+
+    @Test
+    public void cityinPoly() {
+        int seedint = (int) (Math.random() * 1000000);
+        Seed s = new Seed(seedint);
+        //        new Color(123, 183, 64);
+        new ShapeRenderer().Rendering(meshADT1, s);
+        new ElevationRenderer().Rendering(meshADT1, s);
+        new LakeRenderer().Rendering(meshADT1, s);
+        new RiversRenderer().Rendering(meshADT1, s);
+        new BiomeRenderer().Rendering(meshADT1, s);
+
+        for (var temp : meshADT1.getPolygons()) {
+            double x = temp.getCentroid().getX();
+            double y = temp.getCentroid().getY();
+            SegmentADT s1 = meshADT1.getSegment(meshADT1.getVertex(x, y), meshADT1.getVertex(x, y));
+            s1.setColor(new int[]{123, 183, 64});
+            s1.setThickness(30);
+        }
 
 
     }
+
+
     @BeforeEach
     void initial() throws IOException {
         meshADT1 = new MeshADT();
