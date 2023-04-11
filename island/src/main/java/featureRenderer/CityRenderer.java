79ec34fe57d3a82ec2ff853dce214334dfb11936
xiaotian Lou <loux8@mcmaster.ca>
2023-04-11 19:42:56 -0400

    Feature city:CityRenderern

---
+++ island/src/main/java/featureRenderer/CityRenderer.java
@@ -0,0 +1,11 @@
+package featureRenderer;
+
+import Reproducibility.Seed;
+import transformation.builtinADT.MeshADT;
+
+public class CityRenderer implements Renderable{
+    @Override
+    public MeshADT Rendering(MeshADT m, Seed seed) {
+        return null;
+    }
+}
