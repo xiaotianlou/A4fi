aaa97ad477b8b691554afcedd91cd070c1d0f36f
xiaotian Lou <loux8@mcmaster.ca>
2023-04-12 02:41:10 -0400

    StarNetworkRenderer

---
+++ island/src/main/java/featureRenderer/City/StarNetworkRenderer.java
@@ -0,0 +1,12 @@
+package featureRenderer.City;
+
+import Reproducibility.Seed;
+import featureRenderer.Renderable;
+import transformation.builtinADT.MeshADT;
+
+public class StarNetworkRenderer implements Renderable {
+    @Override
+    public MeshADT Rendering(MeshADT m, Seed seed) {
+        return null;
+    }
+}
